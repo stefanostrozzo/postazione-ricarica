a959641798bf389c992a9c67a484974eccf62d7e
Stefano <122939470+stefanostrozzo@users.noreply.github.com>
2024-06-11 11:42:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,7 @@ debug=False
 
 if __name__ == "__main__":
     setup()
-    while True:
-        result1= get_distance(TRIG_PIN_1,ECHO_PIN_1)
-        result2= get_distance(TRIG_PIN_2,ECHO_PIN_2)
-        result3= get_distance(TRIG_PIN_3,ECHO_PIN_3)
-
-        if debug:
-            print(f"\nSensore 1: {result1}")
-            print(f"\nSensore 2: {result2}")
-            print(f"\nSensore 3: {result3}")
-
+    print(TRIG_PIN_1)
 
 
 
